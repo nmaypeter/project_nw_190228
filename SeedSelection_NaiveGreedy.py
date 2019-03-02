ff1a184d30ca8632f6d8de1e9e7c4f43137a0d6d
nmaypeter <37822464+nmaypeter@users.noreply.github.com>
2019-03-02 16:10:13 +0800

    Update SeedSelection_NaiveGreedy.py
    1. 更新celf寫法，使用比較mg來選seed。
    2. mep_g都用pop出來的。
    3. 後移eva、w_list、pp_list。
    4. now_budget <= total_budget。

--- SeedSelection_NaiveGreedy.py
+++ SeedSelection_NaiveGreedy.py
@@ -18,86 +18,12 @@ class SeedSelectionNG:
         self.num_product = len(prod_list)
         self.monte = monte
 
-    def getMarginalProfit(self, k_prod, i_node, s_set):
-        # -- calculate the expected profit for single node when i_node's chosen as a seed for k-product --
-        ### ep: (float2) the expected profit
-        s_set_t = copy.deepcopy(s_set)
-        s_set_t[k_prod].add(i_node)
-        a_n_set = copy.deepcopy(s_set_t)
-        a_e_set = [{} for _ in range(self.num_product)]
-        ep = 0.0
-
-        # -- notice: prevent the node from owing no receiver --
-        if i_node not in self.graph_dict:
-            return round(ep, 4)
-
-        # -- insert the children of seeds into try_s_n_sequence --
-        ### try_s_n_sequence: (list) the sequence to store the seed for k-products [k, i]
-        ### try_a_n_sequence: (list) the sequence to store the nodes may be activated for k-products [k, i, prob]
-        try_s_n_sequence, try_a_n_sequence = [], []
-        for k in range(self.num_product):
-            for i in s_set_t[k]:
-                try_s_n_sequence.append([k, i])
-
-        while len(try_s_n_sequence) > 0:
-            seed = choice(try_s_n_sequence)
-            try_s_n_sequence.remove(seed)
-            k_prod_t, i_node_t = seed[0], seed[1]
-
-            out_dict = self.graph_dict[i_node_t]
-            for out in out_dict:
-                if random.random() > float(out_dict[out]):
-                    continue
-
-                if out in a_n_set[k_prod_t]:
-                    continue
-                if i_node in a_e_set[k_prod_t] and out in a_e_set[k_prod_t][i_node]:
-                    continue
-                try_a_n_sequence.append([k_prod_t, out, 1])
-                a_n_set[k_prod_t].add(i_node_t)
-                if i_node_t in a_e_set[k_prod_t]:
-                    a_e_set[k_prod_t][i_node_t].add(out)
-                else:
-                    a_e_set[k_prod_t][i_node_t] = {out}
-
-        while len(try_a_n_sequence) > 0:
-            try_node = choice(try_a_n_sequence)
-            try_a_n_sequence.remove(try_node)
-            k_prod_t, i_node_t, child_depth = try_node[0], try_node[1], try_node[2]
-
-            ### -- purchasing --
-            ep += self.product_list[k_prod_t][0]
-
-            # -- notice: prevent the node from owing no receiver --
-            if i_node_t not in self.graph_dict:
-                continue
-
-            if child_depth >= 3:
-                continue
-
-            out_dict = self.graph_dict[i_node_t]
-            for out in out_dict:
-                if random.random() > float(out_dict[out]):
-                    continue
-
-                if out in a_n_set[k_prod_t]:
-                    continue
-                if i_node in a_e_set[k_prod_t] and out in a_e_set[k_prod_t][i_node]:
-                    continue
-                try_a_n_sequence.append([k_prod_t, out, child_depth + 1])
-                a_n_set[k_prod_t].add(i_node_t)
-                if i_node_t in a_e_set[k_prod_t]:
-                    a_e_set[k_prod_t][i_node_t].add(out)
-                else:
-                    a_e_set[k_prod_t][i_node_t] = {out}
-
-        return round(ep, 4)
-
     def generateCelfSequence(self):
         # -- calculate expected profit for all combinations of nodes and products --
-        celf_seq = [[-1, '-1', 0.0, 0.0]]
+        ### celf_ep: (list) [k_prod, i_node, mg, flag]
+        celf_seq = [[-1, '-1', 0.0, 0]]
 
-        ssng_ss = SeedSelectionNG(self.graph_dict, self.seed_cost_dict, self.product_list, self.total_budget, self.monte)
+        diff_ss = Diffusion(self.graph_dict, self.seed_cost_dict, self.product_list, self.total_budget, self.monte)
 
         for k in range(self.num_product):
             for i in set(self.graph_dict.keys()):
@@ -108,85 +34,20 @@ class SeedSelectionNG:
                 s_set = [set() for _ in range(self.num_product)]
                 s_set[k].add(i)
                 ep = 0.0
-
                 for _ in range(self.monte):
-                    ep += ssng_ss.getMarginalProfit(k, i, s_set)
+                    ep += diff_ss.getSeedSetProfit(s_set)
                 ep = round(ep / self.monte, 4)
                 mg = round(ep, 4)
-                mg_ratio = 0.0
-                if self.seed_cost_dict[i] != 0:
-                    mg_ratio = round(mg / self.seed_cost_dict[i], 4)
 
-                if mg <= 0:
-                    continue
-                celf_ep = [k, i, mg, mg_ratio]
+                celf_ep = [k, i, mg, 0]
                 celf_seq.append(celf_ep)
                 for celf_item in celf_seq:
-                    if celf_ep[3] >= celf_item[3]:
+                    if celf_ep[2] >= celf_item[2]:
                         celf_seq.insert(celf_seq.index(celf_item), celf_ep)
                         celf_seq.pop()
                         break
 
-        celf_seq.remove([-1, '-1', 0.0, 0.0])
-        mep = celf_seq[0]
-
-        return mep, celf_seq
-
-    def getMostValuableSeed(self, s_set, celf_seq_ini, cur_pro, cur_bud):
-        # -- calculate expected profit for all combinations of nodes and products --
-        celf_seq = [[-1, '-1', 0.0, 0.0]]
-
-        ssng_ss = SeedSelectionNG(self.graph_dict, self.seed_cost_dict, self.product_list, self.total_budget, self.monte)
-
-        # -- the cost of seed cannot exceed the budget --
-        if self.seed_cost_dict[celf_seq_ini[0][1]] + cur_bud <= self.total_budget and len(celf_seq_ini) >= 2:
-            ep_ini_top = 0.0
-            for _ in range(self.monte):
-                ep_ini_top += ssng_ss.getMarginalProfit(celf_seq_ini[0][0], celf_seq_ini[0][1], s_set)
-            ep_ini_top = round(ep_ini_top / self.monte, 4)
-            mg_ini = round(ep_ini_top - cur_pro, 4)
-            mg_ini_ratio = 0.0
-            if self.seed_cost_dict[celf_seq_ini[0][1]] != 0:
-                mg_ini_ratio = round(mg_ini / self.seed_cost_dict[celf_seq_ini[0][1]], 4)
-
-            if mg_ini_ratio >= celf_seq_ini[1][3]:
-                mep = [celf_seq_ini[0][0], celf_seq_ini[0][1], mg_ini, mg_ini_ratio]
-
-                return mep, celf_seq_ini
-
-        for celf in celf_seq_ini:
-            k_prod, i_node = celf[0], celf[1]
-            ep = 0.0
-
-            # -- the cost of seed cannot exceed the budget --
-            if self.seed_cost_dict[i_node] + cur_bud > self.total_budget:
-                continue
-
-            for _ in range(self.monte):
-                ep += ssng_ss.getMarginalProfit(k_prod, i_node, s_set)
-            ep = round(ep / self.monte, 4)
-            mg = round(ep - cur_pro, 4)
-            mg_ratio = 0.0
-            if self.seed_cost_dict[i_node] != 0:
-                mg_ratio = round(mg / self.seed_cost_dict[i_node], 4)
-
-            if mg <= 0:
-                continue
-            celf_ep = [k_prod, i_node, mg, mg_ratio]
-            celf_seq.append(celf_ep)
-            for celf_item in celf_seq:
-                if celf_ep[3] >= celf_item[3]:
-                    celf_seq.insert(celf_seq.index(celf_item), celf_ep)
-                    celf_seq.pop()
-                    break
-
-        celf_seq.remove([-1, '-1', 0.0, 0.0])
-        if len(celf_seq) == 0:
-            mep = [-1, '-1', 0.0, 0.0]
-        else:
-            mep = celf_seq[0]
-
-        return mep, celf_seq
+        return celf_seq
 
 
 if __name__ == "__main__":
@@ -195,7 +56,7 @@ if __name__ == "__main__":
     total_budget = 10
     pp_strategy = 1
     whether_passing_information_without_purchasing = bool(0)
-    monte_carlo, eva_monte_carlo = 100, 100
+    monte_carlo, eva_monte_carlo = 10, 100
 
     iniG = IniGraph(data_set_name)
     iniW = IniWallet(data_set_name)
@@ -204,17 +65,13 @@ if __name__ == "__main__":
     seed_cost_dict = iniG.constructSeedCostDict()
     graph_dict = iniG.constructGraphDict()
     product_list = iniP.getProductList()
-    wallet_list = iniW.getWalletList(product_name)
     num_node = len(seed_cost_dict)
     num_product = len(product_list)
 
     # -- initialization for each budget --
     start_time = time.time()
-
     ssng = SeedSelectionNG(graph_dict, seed_cost_dict, product_list, total_budget, monte_carlo)
-    eva = Evaluation(graph_dict, seed_cost_dict, product_list, pp_strategy, whether_passing_information_without_purchasing)
-
-    personal_prob_list = eva.setPersonalProbList(wallet_list)
+    diff = Diffusion(graph_dict, seed_cost_dict, product_list, total_budget, monte_carlo)
 
     ### result: (list) [profit, budget, seed number per product, customer number per product, seed set] in this execution_time
     result = []
@@ -223,27 +80,56 @@ if __name__ == "__main__":
     profit_k_list, budget_k_list = [0.0 for _ in range(num_product)], [0.0 for _ in range(num_product)]
 
     # -- initialization for each sample_number --
-    ### now_profit, now_budget: (float) the profit and budget in this execution_time
-    now_profit, now_budget = 0.0, 0.0
+    ### now_budget: (float) the budget in this execution_time
+    now_budget = 0.0
     ### seed_set: (list) the seed set
     seed_set = [set() for _ in range(num_product)]
 
-    mep_g, celf_sequence = ssng.generateCelfSequence()
-    mep_k_prod, mep_i_node, mep_profit = mep_g[0], mep_g[1], mep_g[2]
+    celf_sequence = ssng.generateCelfSequence()
+    mep_g = celf_sequence.pop(0)
+    mep_k_prod, mep_i_node, mep_mg, mep_flag = mep_g[0], mep_g[1], mep_g[2], mep_g[3]
 
-    # -- main --
-    while now_budget < total_budget and mep_i_node != '-1':
-        celf_sequence.remove(celf_sequence[0])
-        seed_set[mep_k_prod].add(mep_i_node)
+    while now_budget <= total_budget and mep_i_node != '-1':
+        if now_budget + seed_cost_dict[mep_i_node] > total_budget:
+            mep_g = celf_sequence.pop(0)
+            mep_k_prod, mep_i_node, mep_mg, mep_flag = mep_g[0], mep_g[1], mep_g[2], mep_g[3]
+            continue
 
-        budget_k_list[mep_k_prod] += seed_cost_dict[mep_i_node]
-        now_profit += mep_profit
-        now_budget += seed_cost_dict[mep_i_node]
+        seed_set_length = sum(len(seed_set[kk]) for kk in range(num_product))
+        if mep_flag == seed_set_length:
+            seed_set[mep_k_prod].add(mep_i_node)
 
-        if len(celf_sequence) == 0:
-            break
-        mep_g, celf_sequence = ssng.getMostValuableSeed(seed_set, celf_sequence, now_profit, now_budget)
-        mep_k_prod, mep_i_node, mep_profit = mep_g[0], mep_g[1], mep_g[2]
+            budget_k_list[mep_k_prod] += seed_cost_dict[mep_i_node]
+            now_budget += seed_cost_dict[mep_i_node]
+        else:
+            ep_g = 0.0
+            for _ in range(monte_carlo):
+                ep_g += diff.getSeedSetProfit(seed_set)
+            ep_g = round(ep_g / monte_carlo, 4)
+
+            ep1_g = 0.0
+            for _ in range(monte_carlo):
+                ep1_g += diff.getExpectedProfit(mep_k_prod, mep_i_node, seed_set)
+            ep1_g = round(ep1_g / monte_carlo, 4)
+            mep_mg = round(ep1_g - ep_g, 4)
+
+            seed_set_length = sum(len(seed_set[kk]) for kk in range(num_product))
+            mep_flag = seed_set_length
+
+            celf_ep_g = [mep_k_prod, mep_i_node, mep_mg, mep_flag]
+            celf_sequence.append(celf_ep_g)
+            for celf_item_g in celf_sequence:
+                if celf_ep_g[2] >= celf_item_g[2]:
+                    celf_sequence.insert(celf_sequence.index(celf_item_g), celf_ep_g)
+                    celf_sequence.pop()
+                    break
+
+        mep_g = celf_sequence.pop(0)
+        mep_k_prod, mep_i_node, mep_mg, mep_flag = mep_g[0], mep_g[1], mep_g[2], mep_g[3]
+
+    eva = Evaluation(graph_dict, seed_cost_dict, product_list, pp_strategy, whether_passing_information_without_purchasing)
+    wallet_list = iniW.getWalletList(product_name)
+    personal_prob_list = eva.setPersonalProbList(wallet_list)
 
     pro_acc, pro_k_list_acc, pnn_k_list_acc = 0.0, [0.0 for _ in range(num_product)], [0 for _ in range(num_product)]
     for _ in range(eva_monte_carlo):
